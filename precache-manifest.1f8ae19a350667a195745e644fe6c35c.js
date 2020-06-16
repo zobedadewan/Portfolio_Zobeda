52cd5817ce0151e7259cc834b9b54339bb00f935
zobedadewan <dewan.zobed@gmail.com>
2020-06-16 17:47:03 -0400

    Updates

---
+++ precache-manifest.1f8ae19a350667a195745e644fe6c35c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f248b578540dceee80fd50d99de64cb",
+    "url": "/portfolio_zobeda/index.html"
+  },
+  {
+    "revision": "89e553aea75073aee11f",
+    "url": "/portfolio_zobeda/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "631ad5f0d51a2ea3dd07",
+    "url": "/portfolio_zobeda/static/css/main.b32c2155.chunk.css"
+  },
+  {
+    "revision": "89e553aea75073aee11f",
+    "url": "/portfolio_zobeda/static/js/2.172ad7e7.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio_zobeda/static/js/2.172ad7e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631ad5f0d51a2ea3dd07",
+    "url": "/portfolio_zobeda/static/js/main.f2fbb9ad.chunk.js"
+  },
+  {
+    "revision": "a5ed579918121ed9eda3",
+    "url": "/portfolio_zobeda/static/js/runtime-main.6446ddbc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e46ed11d6829accc8cff3bdf46600905.js"
+  "/portfolio_zobeda/precache-manifest.1f8ae19a350667a195745e644fe6c35c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio_zobeda/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
